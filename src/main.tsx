430dac5ddfcd53f690ec9b2054902378de96887f
Hoai Thanh Pham Truong <hoai-thanh.pham-truong@ekino.com>
2024-11-27 01:32:35 +0700

    chore(): using launchdarkly

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { withLDProvider } from "launchdarkly-react-client-sdk";
+
+const clientSideID = import.meta.env.VITE_LAUNCHDARKLY_CLIENT_ID;
+
+const LDApp = withLDProvider({
+  clientSideID: clientSideID,
+})(() => (
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+));
+
+createRoot(document.getElementById("root")!).render(<LDApp />);
--- src/pages/HomePage/Homepage.tsx
+++ src/pages/HomePage/Homepage.tsx
@@ -1,12 +1,14 @@
 import Card from "../../ui/Card/Card";
-import { ENV } from "../../consts/env";
+import { useFlags } from "launchdarkly-react-client-sdk";
 
 const Homepage = () => {
+  const { sampleFeature } = useFlags();
+
   return (
     <div className="flex min-h-screen w-screen justify-center items-center gap-6">
       <Card title="Feature A" className="dark:bg-red-500" />
       <Card title="Feature B" className="dark:bg-red-500" />
-      {ENV.ENABLE_FEATURE_C === "true" && <Card title="Feature C" />}
+      {sampleFeature && <Card title="Feature C" />}
     </div>
   );
 };
